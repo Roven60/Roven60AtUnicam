9e78b745c4194c63bf9bbe5b4e4166afd57366e2
Roven60 <roberto.venturi@studenti.unicam.it>
2025-11-10 20:41:13 +0100

    Esercizi del 10/11/2025

--- src/Lezioni/Cap_12_Permutazioni.java
+++ src/Lezioni/Cap_12_Permutazioni.java
@@ -3,39 +3,95 @@ package Lezioni;
 import java.util.Arrays;
 
 public class Cap_12_Permutazioni {
-  /*
-    Esercizio per il 03/11/2025
-   */
 
-  // Restituisce l'array fornito, con l'elemento k inserito in poszione i //
-  static int[] inserisci(int k, int[] a, int i) {
-    if (i > a.length) return null;
-    int[] a2 = new int[a.length + 1];
-    for (int j = 0; j < i; j++) a2[j] = a[j];
-    a2[i] = k;
-    for (int j = i; j < a.length; j++) a2[j + 1] = a[j];
-    return a2;
+  //Scritta dal prof durante la lezione: necessito della registrazione per riscriverla
+  public static int[] aggiungiElemento(int[] a, int j, int v) {
+    return inserisci(v, a, j);
   }
 
-  static int[][] permutazioni(int n) {
-    if (n == 1) {
-      return new int[][]{{1}};
+  //Scritta dal prof durante la lezione: necessito della registrazione per riscriverla
+  public static int[][] generaPermutazioni(int[] a) {
+    int[][] permutazioni = new int[][]{{}};
+    int[][] nuovePermutazioni = null;
+    for (int i = 0; i < a.length; i++) {
+      nuovePermutazioni = new int[permutazioni.length * (i + 1)][];
+      int w = 0;
+      for (int j = 0; j < permutazioni.length; j++) {
+        for (int k = 0; k < permutazioni[j].length; k++) {
+          nuovePermutazioni[w++] = aggiungiElemento(permutazioni[j], k, a[i]);
+        }
+      }
     }
-    int[][] sub = permutazioni(n - 1);
+    return nuovePermutazioni;
+  }
+
+  // return given array (1 item bigger)
+  // with "value" inserted in position idx
+  static int[] inserisci(int value, int[] a, int idx) {
+    if (idx > a.length) return null;
+    int[] result = new int[a.length + 1];
+    for (int j = 0; j < idx; j++) result[j] = a[j];
+    result[idx] = value;
+    for (int j = idx; j < a.length; j++) result[j + 1] = a[j];
+    return result;
+  }
+
+  static int[][] permutazioniIndice(int n) {
+    if (n < 0) {
+      return null;
+    }
+    if (n == 0) {
+      return new int[][]{{}};
+    }
+    // recursively evaluate method with 1 less index
+    int[][] sub = permutazioniIndice(n - 1);
+    assert sub != null; //to avoid compiler warning
+    // now create a new (external) bigger array
     int[][] res = new int[sub.length * n][];
+    // and fill with shorter array more new index in every position
     for (int ri = 0; ri < n; ri++) {
       for (int si = 0; si < sub.length; si++) {
         res[n * si + ri] = inserisci(n, sub[si], ri);
+      }
+    }
+    return res;
+  }
 
+  static int[][] permutazioniContenuto(int[] inta) {
+    if (inta == null) {
+      return null;
+    }
+    if (inta.length == 0) {
+      return new int[][]{{}};
+    }
+    // recursively evaluate method with an array without last element
+    int[] intaPrev = new int[inta.length - 1];
+    for (int i = 0; i < intaPrev.length; i++)
+      intaPrev[i] = inta[i];
+    int[][] sub = permutazioniContenuto(intaPrev);
+    // now create a new (external) bigger array
+    assert sub != null; //to avoid compiler warning
+    int[][] res = new int[sub.length * inta.length][];
+    // and fill with shorter array more new element in every position
+    for (int ii = 0; ii < inta.length; ii++) {
+      for (int si = 0; si < sub.length; si++) {
+        res[inta.length * si + ii] = inserisci(inta[inta.length - 1], sub[si], ii);
       }
     }
     return res;
   }
 
   static void main(String[] args) {
-    int[][] res = permutazioni(4);
-    for (int i = 0; i < res.length; i++) {
-      System.out.println(Arrays.toString(res[i]));
+    int[][] r1 = permutazioniIndice(3);
+    for (int i = 0; i < r1.length; i++) {
+      System.out.println(Arrays.toString(r1[i]));
+    }
+
+    System.out.println("=====");
+    int[] r2 = {15, -37, 21};
+    int[][] r3 = permutazioniContenuto(r2);
+    for (int i = 0; i < r3.length; i++) {
+      System.out.println(Arrays.toString(r3[i]));
     }
   }
 
---
+++ src/Lezioni/Slide_13_ClassiOggetti.java
@@ -0,0 +1,174 @@
+package Lezioni;
+
+import Progetti_Personali._RvLib;
+
+public class Slide_13_ClassiOggetti {
+
+  public static class Orologio {
+    int ore = 0;
+    int minuti = 0;
+
+    public Orologio(int ore, int minuti) {
+      if (ore > 0 && ore < 23) {
+        this.ore = ore;
+      }
+      if (minuti > 0 && minuti < 59) {
+        this.minuti = minuti;
+      }
+    }
+
+    public int get_ora() {
+      return ore;
+    }
+
+    public int get_minuti() {
+      return minuti;
+    }
+
+    public void tick() {
+      minuti++;
+      if (minuti > 59) {
+        ore++;
+        minuti = 0;
+      }
+      if (ore > 23) {
+        ore = 0;
+      }
+    }
+
+  } //Orologio end
+
+  public static class Recipiente {
+    private int volume;     // 0 <= volume private int contenuto;
+    private int contenuto;  // 0 <= contenuto <= volume ...
+    private boolean aperto;
+
+    public Recipiente(int quantita) { // quantita >= 0
+      volume = quantita;
+      contenuto = 0;
+      aperto = false;
+    }
+
+    public int get_volume() {
+      return volume;
+    }
+
+    public int get_contenuto() {
+      return contenuto;
+    }
+
+    public int capacita() {
+      return volume - contenuto;
+    }
+
+    public void apri() {
+      aperto = true;
+    }
+
+    public void chiudi() {
+      aperto = false;
+    }
+
+    public void aggiungi(int quantita) { // 0 <= quantita
+      if (aperto)
+        contenuto = Math.min(contenuto + quantita, volume);
+    }
+
+    public void rimuovi(int quantita) { // 0 <= quantita
+      if (aperto)
+        contenuto = Math.max(contenuto - quantita, 0);
+    }
+  }  //Recipiente End
+
+  public static class ContoCorrente {
+    private int saldo;
+    private int saldoMax;
+
+    public ContoCorrente(int saldo) {
+      this.saldo = saldo;
+      this.saldoMax = saldo;
+    }
+
+    public int get_saldo() {
+      return saldo;
+    }
+
+    public int get_saldoMax() {
+      return saldoMax;
+    }
+
+    public void deposita(int cifra) {
+      this.saldo += cifra;
+      if (saldo > saldoMax)
+        this.saldoMax = saldo;
+    }
+
+    public int preleva(int cifra) {
+      int disponibilita = Math.min(saldo, cifra);
+      this.saldo -= disponibilita;
+      return disponibilita;
+    }
+  } //ContoCorrente End
+
+  public static class Primes {
+    private int current;
+
+    public Primes() {
+      current = 1;
+    }
+
+    public int next() {
+      for (int i = current + 1; i < Integer.MAX_VALUE; i++) {
+        if (this.isPrime(i)) {
+          current = i;
+        }  //else try next integer
+      }
+      //all primes < MAX_VALUE returned: what to do?
+      return current;
+    }
+
+    private boolean isPrime(int number) {
+      int i;
+      for (i = 2; i <= number / i; i++)
+        if (number % i == 0) break;
+      return (i > number / i);
+    }
+
+  } //Primes End
+
+
+  static void main(String[] args) {
+    StdOut.println("===== OROLOGIO =====");
+    Orologio o1 = new Orologio(20, 58);
+    StdOut.println("Ore " + o1.get_ora() + ":" + o1.get_minuti());
+    o1.tick();
+    StdOut.println("Ore " + o1.get_ora() + ":" + o1.get_minuti());
+    o1.tick();
+    StdOut.println("Ore " + o1.get_ora() + ":" + o1.get_minuti());
+    StdOut.println("===== RECIPIENTE =====");
+    Recipiente r1 = new Recipiente(10);
+    StdOut.println("Capacità " + r1.capacita());
+    r1.aggiungi(4);
+    StdOut.println("Capacità " + r1.capacita());
+    r1.apri();
+    r1.aggiungi(4);
+    StdOut.println("Capacità " + r1.capacita());
+    StdOut.println("===== CONTO CORRENTE =====");
+    ContoCorrente cc1 = new ContoCorrente(20);
+    StdOut.println("Saldo " + cc1.get_saldo() + " (max " + cc1.get_saldoMax() +")");
+    cc1.deposita(2);
+    StdOut.println("Saldo " + cc1.get_saldo() + " (max " + cc1.get_saldoMax() +")");
+    cc1.deposita(8);
+    StdOut.println("Saldo " + cc1.get_saldo() + " (max " + cc1.get_saldoMax() +")");
+    int contante = cc1.preleva(18);
+    StdOut.println("Saldo " + cc1.get_saldo() + " (max " + cc1.get_saldoMax() +") contante " + contante);
+    contante = cc1.preleva(18);
+    StdOut.println("Saldo " + cc1.get_saldo() + " (max " + cc1.get_saldoMax() +") contante " + contante);
+    StdOut.println("===== PRIMES =====");
+    Primes p1 = new Primes();
+    for (int i = 0; i < 10; i++) {
+      StdOut.println("Next prime is " + p1.next());
+    }
+  }
+
+}
